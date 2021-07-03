ff689d4f39fcebdb15d359099ed17cd57bdb7012
Ikram Hasan <ikramhasan.dev@gmail.com>
2021-07-04 04:48:50 +0600

    Added environment port

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,5 @@
 const app = require("./app");
 
-app.listen(3000, () => {
-  console.log("Server running on port 3000");
+app.listen(process.env.PORT || 5000, () => {
+  console.log("Server running on port 5000");
 });
