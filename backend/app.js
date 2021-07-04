92636ca571d6fed3272900f4656e2f4aee98bd84
Ikram Hasan <ikramhasan.dev@gmail.com>
2021-07-04 22:43:44 +0600

    added cors

--- backend/app.js
+++ backend/app.js
@@ -7,7 +7,7 @@ app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000/");
+  res.header("Access-Control-Allow-Origin", "*");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
