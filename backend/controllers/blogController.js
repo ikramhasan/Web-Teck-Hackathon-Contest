e965d5cd628247aae70b6ad1b330706db5fa5c09
Ikram Hasan <ikramhasan.dev@gmail.com>
2021-07-04 21:18:57 +0600

    Added create blog functionality

--- backend/controllers/blogController.js
+++ backend/controllers/blogController.js
@@ -1,4 +1,5 @@
 const db = require("../databaseConnection");
+const readingTime = require("reading-time");
 
 exports.getAllBlogs = (req, res) => {
   const sql =
@@ -18,4 +19,38 @@ exports.getAllBlogs = (req, res) => {
       data: result,
     });
   });
+};
+
+exports.createBlog = (req, res) => {
+  const sql = "INSERT INTO blog SET ?";
+  const blog = req.body;
+  const stats = readingTime(blog.caption);
+
+  db.query(
+    sql,
+    {
+      caption: blog.caption,
+      user_id: blog.user_id,
+      vote_count: 0,
+      image_url: blog.image_url,
+      createdAt: Date.now(),
+      tag: blog.tag,
+      time_to_read: stats.text,
+    },
+    (err, result) => {
+      if (err) {
+        res.status(500).json({
+          status: "fail",
+          message:
+            err.code === "ER_DUP_ENTRY" ? "User already exists!" : err.message,
+        });
+      }
+
+      res.status(201).json({
+        status: "success",
+        // token,
+        data: result,
+      });
+    }
+  );
 };
