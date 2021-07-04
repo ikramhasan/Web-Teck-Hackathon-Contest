b1e72a5b6697d1a1f1619f4bfaa6eb6eb82532b8
Shehjad Ali <satauskz@gmail.com>
2021-07-04 21:25:01 +0600

    Front end

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,5 @@
 const app = require("./app");
 
-app.listen(3000, () => {
-  console.log("Server running on port 3000");
+app.listen(5000, () => {
+  console.log("Server running on port 5000");
 });
